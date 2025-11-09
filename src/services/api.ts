de9265b5dea9078fd9916c37265281872926cb79
Vinicius <viniciussantanna2005@gmail.com>
2025-11-09 17:47:56 -0300

    ajustando api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.15.10:8080"; 
+const API_BASE_URL = "http://localhost:8080"; 
 
 const api = axios.create({
   baseURL: API_BASE_URL, 
@@ -23,34 +23,24 @@ interface Localizacao {
   dataHora: string;
 }
 
-// =======================
-// Funções para a Entidade MOTOS (CRUD Completo)
-// =======================
-
-// C: Create
 export const createMoto = (data: Moto) => api.post("/motos", data);
-// R: Read (Todos)
+
 export const getMotos = () => api.get<Moto[]>("/motos");
-// R: Read (Individual)
+
 export const getMoto = (id: number) => api.get<Moto>(`/motos/${id}`);
-// U: Update
+
 export const updateMoto = (id: number, data: Moto) => api.put(`/motos/${id}`, data);
-// D: Delete
+
 export const deleteMoto = (id: number) => api.delete(`/motos/${id}`);
 
 
-// =======================
-// Funções para a Entidade LOCALIZAÇÕES (CRUD Completo)
-// =======================
-
-// C: Create
 export const createLocalizacao = (data: Localizacao) => api.post("/localizacoes", data);
-// R: Read (Todos)
+
 export const getLocalizacoes = () => api.get<Localizacao[]>("/localizacoes");
-// U: Update
+
 export const updateLocalizacao = (id: number, data: Localizacao) =>
   api.put(`/localizacoes/${id}`, data);
-// D: Delete
+
 export const deleteLocalizacao = (id: number) => api.delete(`/localizacoes/${id}`);
 
 export default api;
